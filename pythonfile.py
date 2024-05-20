8e6cda133210cac41fb4a782c6715c404777657c
Gokul <gokul.u2023ai-ds@sece.ac.in>
2024-05-20 15:58:11 +0530

    updated

--- pythonfile.py
+++ pythonfile.py
@@ -1,6 +1,11 @@
 import numpy as np;
 a=np.array([1,2,3])
+#2D array
 b=np.array([[1,2,3],[4,5,6]])
 print(a)
-#2D array
 print(b)
+print(b.ndim)
+print(b.shape)
+print(b.dtype)
+print(b[0,:])
+print(b[:,2])
